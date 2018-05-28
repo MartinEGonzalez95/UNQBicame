3db85a20653f1af43111ae978dd3722deb82182a
Martin Ezequiel Gonzalez <martinegonzalez95@gmail.com>
2018-05-27 23:30:36 -0300

    Realizada la integracion con Spring y un test de concepto

---
+++ src/main/java/UNQBicame/iisoft/tpi/unq/edu/ar/UNQBicame/UnqBicameApplication.java
@@ -0,0 +1,12 @@
+package UNQBicame.iisoft.tpi.unq.edu.ar.UNQBicame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnqBicameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnqBicameApplication.class, args);
+	}
+}
---
+++ src/test/java/Prueba.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import org.springframework.util.Assert;
+
+import static org.junit.Assert.assertEquals;
+
+public class Prueba {
+
+    @Test
+    public void unoMasUnoEsDos() {
+        assertEquals(1+1, 2);
+    }
+
+}
