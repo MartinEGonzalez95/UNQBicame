812f4011b5304dceabe9e15ed22f4622473a7c30
German Cigala <cigalagerman@gmail.com>
2018-06-07 11:29:56 -0300

    Define metodo create()

--- app/Http/Controllers/Web/AulasController.php
+++ app/Http/Controllers/Web/AulasController.php
@@ -7,4 +7,45 @@ use App\Http\Controllers\Controller;
 class AulasController extends Controller
 {
     
+    public function create()
+    {
+        
+        $sectores = [
+            
+            'A' => [
+                'Planta baja',
+                '1er Piso',
+                '2do Piso',
+            ],
+            'B' => [
+                'Planta baja',
+                '1er Piso',
+                '2do Piso',
+            ],
+            'C' => [
+                'Planta baja',
+                '1er Piso',
+            ],
+            'D' => [
+                '1er Piso'
+            ],
+            'E' => [
+                'Planta baja'
+            ],
+            'F' => [
+                'Planta baja',
+                '1er Piso',
+            ],
+            'G' => [
+                'Planta baja',
+                '1er Piso',
+                '2do Piso',
+            ],
+            
+        ];
+        
+        return view('aulas.create')->with(['sectores' => $sectores]);
+        
+    }
+    
 }
