16ef74a04093e5d2e34cfd57c46f73f277ef3348
German Cigala <cigalagerman@gmail.com>
2018-06-07 11:40:40 -0300

    Agrega nombre a la ruta /aulas/crear

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/aulas/agregar', 'Web\AulasController@create');
+Route::get('/aulas/agregar', 'Web\AulasController@create')->name('aulas.create');
