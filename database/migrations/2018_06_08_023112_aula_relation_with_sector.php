94263232fb3af67c1ba0cfcfe1ecdae83f916bf7
German Cigala <cigalagerman@gmail.com>
2018-06-08 11:57:22 -0300

    Agrega default value a la columna sector_id

--- database/migrations/2018_06_08_023112_aula_relation_with_sector.php
+++ database/migrations/2018_06_08_023112_aula_relation_with_sector.php
@@ -15,7 +15,7 @@ class AulaRelationWithSector extends Migration
     {
         Schema::table('aulas',function (Blueprint $table) {
 
-            $table->integer('sector_id');
+            $table->integer('sector_id')->default(0);
         });
     }
 
