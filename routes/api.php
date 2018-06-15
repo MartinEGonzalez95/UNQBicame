6567f8b56fd79d96f16ab682c75ff605a3546646
Gaston Testay <gaston.testay@gmail.com>
2018-06-15 16:57:24 -0300

    agregada la ruta de api materias.

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/aulas', 'AulasController@index');
+
+Route::get('/materias', 'MateriasControllerAPI@index');
