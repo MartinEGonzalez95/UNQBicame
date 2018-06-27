bd2b61a550f8e55e92421ecca8e728a93b218861
Germán Cigala <cigalagerman@gmail.com>
2018-06-27 12:22:27 -0300

    Define navegacion

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 //Aulas
 
 Route::get('/aulas/agregar', 'Web\AulasController@create')->name('aulas.create');
-Route::get('/aulas', 'Web\AulasController@index');
+Route::get('/aulas', 'Web\AulasController@index')->name('aulas.index');
 Route::post('/aulas/agregar','Web\AulasController@store');
 
 Route::get('/aulas/{id}/editar','Web\AulasController@edit');
@@ -27,7 +27,7 @@ Route::put('/aulas/{id}/editar','Web\AulasController@update');
 
 
 //Materias
-Route::get('/materias', 'Web\MateriasController@index');
+Route::get('/materias', 'Web\MateriasController@index')->name('materias.index');
 
 Route::get('/materias/agregar', 'Web\MateriasController@create')->name('materias.create');
 Route::post('/materias','Web\MateriasController@store');
